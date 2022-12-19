b586c7711ee79b9113c70d6111da0c9d651fed23
rahulbaruah <baruah.rahul.88@gmail.com>
2022-12-19 13:17:10 +0530

    google translate laravel project

---
+++ database/migrations/2022_01_03_102303_create_translations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('translations', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->string('col_name');
+            $table->integer('translationable_id');
+            $table->string('table');
+            $table->string('translationable_type');
+            $table->string('lang_code',5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('translations');
+    }
+}
